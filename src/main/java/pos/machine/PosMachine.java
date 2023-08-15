b0d95b8a4711fd35db8e215808a999fb4746f9a8
AubBantolo <jhuliet14@gmail.com>
2023-08-15 18:44:40 +0800

    feat: called decodeToItems into printReceipt and loaded items to group them and get quantity

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,20 @@
 package pos.machine;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        List<ReceiptItem> receiptItems = decodeToItems(barcodes);
+        return null;
+    }
+
+    private List<ReceiptItem> decodeToItems(List<String> barcodes) {
+        List<Item> items = ItemsLoader.loadAllItems();
+
+        Map<String, Long> itemCountMap = barcodes.stream().collect(Collectors.groupingBy(barcode -> barcode, Collectors.counting()));
         return null;
     }
 }
