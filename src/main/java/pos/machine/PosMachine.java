9c2c0a39245d476b1342ebd2e735533f94bb35ea
AubBantolo <jhuliet14@gmail.com>
2023-08-15 19:38:11 +0800

    feat: created function buildReceiptItem to decode barcodes from items and removed setters from ReceiptItem class

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,6 +1,5 @@
 package pos.machine;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,9 +11,26 @@ public class PosMachine {
     }
 
     private List<ReceiptItem> decodeToItems(List<String> barcodes) {
-        List<Item> items = ItemsLoader.loadAllItems();
+        Map<String, Long> itemCountMap = barcodes.stream()
+                .collect(Collectors.groupingBy(barcode -> barcode, Collectors.counting()));
 
-        Map<String, Long> itemCountMap = barcodes.stream().collect(Collectors.groupingBy(barcode -> barcode, Collectors.counting()));
-        return null;
+        return itemCountMap.entrySet()
+                .stream()
+                .map(itemCountMapEntry -> buildReceiptItem(itemCountMapEntry))
+                .collect(Collectors.toList());
+    }
+
+    private ReceiptItem buildReceiptItem(Map.Entry<String, Long> itemCountMapEntry) {
+        Item entryItem = ItemsLoader.loadAllItems().stream()
+                .filter(item -> item.getBarcode().equals(itemCountMapEntry.getKey()))
+                .findFirst()
+                .orElse(null);
+
+        return entryItem != null
+                ? new ReceiptItem(entryItem.getName()
+                , itemCountMapEntry.getValue().intValue()
+                , entryItem.getPrice()
+                , itemCountMapEntry.getValue().intValue() * entryItem.getPrice())
+                : null;
     }
 }
--- src/main/java/pos/machine/ReceiptItem.java
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -18,31 +18,15 @@ public class ReceiptItem {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 
     public int getUnitPrice() {
         return unitPrice;
     }
 
-    public void setUnitPrice(int unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
     public int getSubTotal() {
         return subTotal;
-    }
-
-    public void setSubTotal(int subTotal) {
-        this.subTotal = subTotal;
     }
 }
